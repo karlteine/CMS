41d9203e68ab2e37d2cf11d2ce629ad977e647de
karl <karlkivisild@gmail.com>
2024-11-07 12:17:25 +0200

    Made routes for logins and sign-ups

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
+// Import routes
+const mainRoutes = require('./routes/mainRoutes.js');
+
 // Set up Handlebars template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -16,10 +19,8 @@ app.engine('hbs', hbs.engine({
 // Serve static files from the public directory
 app.use(express.static('public'));
 
-// Route for the home page
-app.get('/', (req, res) => {
-    res.render('index'); // Renders index.hbs
-});
+// Use the imported routes
+app.use(mainRoutes);
 
 // Start the server
 app.listen(3025, () => {
